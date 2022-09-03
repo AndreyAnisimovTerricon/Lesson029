d3605ab6be7ff3bae346a27f877ebc924ad96ae8
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-03 14:26:15 +0300

    создание массива и его вывод

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int size = 8;
+int [] array = new int [size];
+for (int i = 0; i < array.Length;i++){
+    array[i] = new Random().Next(1,100);
+}
+//foreach(int el in array){
+
+//Console.WriteLine(el);}
+string joinedarray = string.Join(",", array);
+Console.WriteLine(joinedarray);
+
